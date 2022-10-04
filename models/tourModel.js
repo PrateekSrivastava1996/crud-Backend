83f8e77543cef54366556aeb2ba7988ec882ad6b
prateekEminence <prateek.eminence@gmail.com>
2022-10-04 18:18:39 +0530

    Initial commit

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const slugify = require('slugify');
+const slugify = require('slugify')
 // const User = require('./userModel');
 // const validator = require('validator');
 
